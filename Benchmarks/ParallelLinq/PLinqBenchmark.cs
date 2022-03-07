ea054f33036765986dbf66bc26b469f3e9c32e53
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2022-03-07 18:12:04 +0100

    benchmark linq

---
+++ Benchmarks/ParallelLinq/PLinqBenchmark.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections.Concurrent;
+
+namespace ParallelLinq
+{
+    public class PLinqBenchmark
+    {
+        private List<int> data;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = Enumerable.Range(1, 50_000).ToList();
+        }
+
+        [Benchmark]
+        public void Normal_Linq()
+        {
+            var result = new List<int>();
+            foreach (var item in data)
+            {
+                if (EstPremier(item))
+                {
+                    result.Add(item);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void AsParallel_Linq()
+        {
+            var result = new ConcurrentBag<int>();
+            foreach (var item in data.AsParallel())
+            {
+                if (EstPremier(item))
+                {
+                    result.Add(item);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void Parallel_Linq()
+        {
+            var result = new ConcurrentBag<int>();
+            Parallel.ForEach(data, item =>
+            {
+                if (EstPremier(item))
+                {
+                    result.Add(item);
+                }
+            });
+        }
+
+        private static bool EstPremier(int Nombre)
+        {
+            for (int ind = 2; ind < Nombre - 1; ind++)
+            {
+                if (Nombre % ind == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Benchmarks/ParallelLinq/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using ParallelLinq;
+
+BenchmarkRunner.Run<PLinqBenchmark>();
